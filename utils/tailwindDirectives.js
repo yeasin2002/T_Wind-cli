eb7ea19e9a507e8caa3eea4cf240c0a91e4f47cc
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-04-12 09:35:31 +0000

    a bit update

--- utils/tailwindDirectives.js
+++ utils/tailwindDirectives.js
@@ -1,7 +1,5 @@
-const TailwindDirectives = `
- @tailwind base;
+const TailwindDirectives = `@tailwind base;
 @tailwind components;
-@tailwind utilities;
-`;
+@tailwind utilities;`;
 
 export default TailwindDirectives;
